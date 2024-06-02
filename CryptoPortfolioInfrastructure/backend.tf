f3ef60de37a13a56151da4516e2529fce68bc6ae
David Pizzi <david.pizzi@capgemini.com>
2024-06-03 00:29:22 +0100

    fix tfstate

--- CryptoPortfolioInfrastructure/backend.tf
+++ CryptoPortfolioInfrastructure/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "terraformstatedp1456"
-    container_name       = "tfstate"
-    key                  = "crypto-portfolio-manager/CryptoPortfolioInfrastructure.tfstate"
+    resource_group_name   = "terraform-state-rg"
+    storage_account_name  = "terraformstatedp1456"
+    container_name        = "terraform-state"
+    key                   = "terraform.tfstate"
   }
 }
