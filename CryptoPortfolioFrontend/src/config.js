9165fdf5d91683c5dfcfebb2e15d944704fe0337
David Pizzi <david.pizzi@capgemini.com>
2024-06-14 17:20:54 +0100

    lint fix

--- CryptoPortfolioFrontend/src/config.js
+++ CryptoPortfolioFrontend/src/config.js
@@ -4,36 +4,20 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-const config = {
-  NODE_ENV: process.env.NODE_ENV,
-  REACT_APP_AUTH0_DOMAIN:
-    process.env.NODE_ENV === "production"
-      ? "${{REACT_APP_AUTH0_DOMAIN}}"
-      : process.env.REACT_APP_AUTH0_DOMAIN,
-  REACT_APP_AUTH0_CLIENT_ID:
-    process.env.NODE_ENV === "production"
-      ? "${{REACT_APP_AUTH0_CLIENT_ID}}"
-      : process.env.REACT_APP_AUTH0_CLIENT_ID,
-  REACT_APP_AUTH0_AUDIENCE:
-    process.env.NODE_ENV === "production"
-      ? "${{REACT_APP_AUTH0_AUDIENCE}}"
-      : process.env.REACT_APP_AUTH0_AUDIENCE,
-  REACT_APP_AUTH0_SCOPE:
-    process.env.NODE_ENV === "production"
-      ? "${{REACT_APP_AUTH0_SCOPE}}"
-      : process.env.REACT_APP_AUTH0_SCOPE,
-  REACT_APP_API_BASE_URL:
-    process.env.NODE_ENV === "production"
-      ? "${{REACT_APP_API_BASE_URL}}"
-      : process.env.REACT_APP_API_BASE_URL,
-  REACT_APP_AZURE_FUNCTION_URL:
-    process.env.NODE_ENV === "production"
-      ? "${{REACT_APP_AZURE_FUNCTION_URL}}"
-      : process.env.REACT_APP_AZURE_FUNCTION_URL,
-  REACT_APP_AZURE_FUNCTION_CODE:
-    process.env.NODE_ENV === "production"
-      ? "${{REACT_APP_AZURE_FUNCTION_CODE}}"
-      : process.env.REACT_APP_AZURE_FUNCTION_CODE,
-};
+// src/config.js
 
-export default config;
+const config = {
+    NODE_ENV: process.env.NODE_ENV,
+    /* eslint-disable no-template-curly-in-string */
+    REACT_APP_AUTH0_DOMAIN: process.env.NODE_ENV === 'production' ? "${{REACT_APP_AUTH0_DOMAIN}}" : process.env.REACT_APP_AUTH0_DOMAIN,
+    REACT_APP_AUTH0_CLIENT_ID: process.env.NODE_ENV === 'production' ? "${{REACT_APP_AUTH0_CLIENT_ID}}" : process.env.REACT_APP_AUTH0_CLIENT_ID,
+    REACT_APP_AUTH0_AUDIENCE: process.env.NODE_ENV === 'production' ? "${{REACT_APP_AUTH0_AUDIENCE}}" : process.env.REACT_APP_AUTH0_AUDIENCE,
+    REACT_APP_AUTH0_SCOPE: process.env.NODE_ENV === 'production' ? "${{REACT_APP_AUTH0_SCOPE}}" : process.env.REACT_APP_AUTH0_SCOPE,
+    REACT_APP_API_BASE_URL: process.env.NODE_ENV === 'production' ? "${{REACT_APP_API_BASE_URL}}" : process.env.REACT_APP_API_BASE_URL,
+    REACT_APP_AZURE_FUNCTION_URL: process.env.NODE_ENV === 'production' ? "${{REACT_APP_AZURE_FUNCTION_URL}}" : process.env.REACT_APP_AZURE_FUNCTION_URL,
+    REACT_APP_AZURE_FUNCTION_CODE: process.env.NODE_ENV === 'production' ? "${{REACT_APP_AZURE_FUNCTION_CODE}}" : process.env.REACT_APP_AZURE_FUNCTION_CODE,
+    /* eslint-enable no-template-curly-in-string */
+  };
+  
+  export default config;
+  
