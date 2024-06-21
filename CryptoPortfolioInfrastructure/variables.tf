461eb1d0218d4ad865c8a38f397464c44ecc70c3
David Pizzi <david.pizzi@capgemini.com>
2024-06-21 13:13:33 +0100

    fix

--- CryptoPortfolioInfrastructure/variables.tf
+++ CryptoPortfolioInfrastructure/variables.tf
@@ -19,7 +19,7 @@ variable "location" {
 variable "common_suffix" {
   description = "The common suffix to be used. If not provided, a random string will be used."
   type        = string
-  default     = "7h9eax"
+  default     = ""
 }
 
 variable "auth0_domain" {
